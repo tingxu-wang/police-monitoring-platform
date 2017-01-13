4df95c539c4ee70cce68ff234ce5a70822625ac0
tingxu.wang <768165932@qq.com>
2017-01-13 13:40:41 +0800

    开发版caseType暂时设置为0

--- models/list.js
+++ models/list.js
@@ -68,7 +68,8 @@ function List(list){
     userName:'尚未填写',
     phoneNum:'尚未填写',
     idCard:'尚未填写',
-    caseType:6,//尚未填写的案件类型值
+    //caseType:6,//尚未填写的案件类型值
+    caseType:0,//开发版默认值，之后调回6
 
     userComment:'尚未填写',//用户评价
     policeComment:'尚未填写',//民警评价
