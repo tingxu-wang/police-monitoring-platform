5b8cac1726ec39e5da5f6e185d6959a461202a2c
tingxu.wang <768165932@qq.com>
2017-01-13 14:08:58 +0800

    民警端闪退原因查明，数组月结

--- models/list.js
+++ models/list.js
@@ -68,10 +68,10 @@ function List(list){
     userName:'尚未填写',
     phoneNum:'尚未填写',
     idCard:'尚未填写',
-    caseType:6//尚未填写的案件类型值
+    caseType:6,//尚未填写的案件类型值
 
     userComment:'尚未填写',//用户评价
-    policeComment:'尚未填写',//民警评价
+    policeComment:'尚未填写'//民警评价
 
 //转存转移到之后的表单提交的update中
 /*    caseInfo:list.paperOne.q5,
