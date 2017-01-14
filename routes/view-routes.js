44a931f09d170149bf37a5c9d0370e079a8abc44
tingxu.wang <768165932@qq.com>
2017-01-14 19:37:41 +0800

    完整版第一次提交

--- routes/view-routes.js
+++ routes/view-routes.js
@@ -1,10 +1,6 @@
 
 module.exports=function(app){
-  //视图渲染：
-/*  app.get('/views/:filename', (req, res, next)=>{//根目录加载ejs模板
-    //res.render('index',{title:'test'})
-    res.render(`${req.params.filename}`,{title:'views'})
-  });*/
+  /* 视图渲染 */
   app.get('/',(req,res,next)=>{
     res.render('index',{title:'登录',position:'login'})
   })
@@ -23,6 +19,10 @@ module.exports=function(app){
 
   app.get('/views/detail',(req,res,next)=>{
     res.render('detail',{title:'案件详情',position:'detail',policeName:req.query.policeName,id:req.query.id})
+  })
+
+  app.get('/views/historyFilter',(req,res,next)=>{
+    res.render('historyFilter',{title:'民警历史案件',position:'historyFilter'})
   })
 
   /* 私信页面 */
